47bcf10aae0197d6f35246f60d60dedc319f8945
francesco <info.francescobianco@gmail.com>
2024-05-02 18:23:13 +0200

    First commit

--- server.php
+++ server.php
@@ -1,17 +1,35 @@
 <?php
 
-echo "Hello, World!";
-
-$password = getenv('MARIADB_ROOT_PASSWORD');
+$headers = getallheaders();
 $query = file_get_contents('php://input');
+$database = 'mysql';
+$password = getenv('MARIADB_ROOT_PASSWORD');
+$accessToken = null;
 
-$pdo = new PDO("mysql:host=0.0.0.0;dbname=mysql", 'root', $password);
+if (isset($headers['Authorization'])) {
+    $authorizationHeader = $headers['Authorization'];
+    $matches = array();
+    if (preg_match('/Bearer (.+)/', $authorizationHeader, $matches)) {
+        if (isset($matches[1])) {
+            $accessToken = json_decode(base64_decode($matches[1]), true);
+        }
+    }
+}
+
+if (empty($accessToken)) {
+    http_response_code(401);
+    exit;
+}
+
+var_dump($accessToken);
+
+exit;
+
+$pdo = new PDO("mysql:host=0.0.0.0;dbname={$database}", 'root', $password);
 $pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
 
-$unbufferedResult = $pdo->query("SHOW TABLES");
+$unbufferedResult = $pdo->query($query);
 
 foreach ($unbufferedResult as $row) {
     var_dump($row);
 }
-
-var_dump($query);
--- tests/insert-test.sh
+++ tests/insert-test.sh
@@ -4,11 +4,12 @@ docker compose up -d --build --force-recreate
 
 sleep 5
 
+ACCESS_TOKEN=$(echo -n '{"database":"mysql"}' | base64)
+
 echo
 echo "-----------------------------------------"
-curl localhost:6612 -d '
-  SHOW TABLES;
-  SHOW TABLES;
-  '
+curl localhost:6612 \
+  -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+  -d "SHOW TABLES;"
 
 echo
